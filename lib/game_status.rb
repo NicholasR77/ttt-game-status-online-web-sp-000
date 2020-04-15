449b8f3a49261e6b7633d40607983a1d0907c20a
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-15 14:43:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ def position_taken?(board, index)
  !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+# Variables
 WIN_COMBINATIONS = [
   #horizontal
   [0,1,2],
