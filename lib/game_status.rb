9cd2b9ce050a38b41125fa00074a33d502df61af
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-15 14:48:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
     final_winner = ""
-    
+
     index_0 = win_combo[0]
     index_1 = win_combo[1]
     index_2 = win_combo[2]
