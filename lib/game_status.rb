10ba4859df9959070cd8e2b8d60034c1fe79659f
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-15 13:45:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,43 +10,24 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   #horizontal
-  [1,2,3],
-  [4,5,6],
-  [7,8,9],
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
   #vertical
+  [0,3,6],
   [1,4,7],
   [2,5,8],
-  [3,6,9],
   #diagonal
-  [1,5,9],
-  [3,5,7]
+  [0,4,9],
+  [2,4,6]
 ]
 
 # Define the won method
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    win_index_1 = win_combination[4]
-    win_index_2 = win_combination[5]
-    win_index_3 = win_combination[6]
-    win_index_1 = win_combination[7]
-    win_index_2 = win_combination[8]
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    position_4 = board[win_index_4]
-    position_5 = board[win_index_5]
-    position_6 = board[win_index_6]
-    position_7 = board[win_index_7]
-    position_8 = board[win_index_8]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" && position_4 == "X" && position_5 == "X" && position_6 == "X" && position_7 == "X" && position_8 == "X"
-      puts win_combination # return the win_combination indexes that won.
-    else
-      false
+  WIN_COMBINATIONS.each do |win|
+    board.each do |position|
+      if (board[position] == "X")
+        puts "yay"
     end
   end
 end
