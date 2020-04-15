188de3c5bdc3a954d2fd45b36b4f038f92f8a298
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-15 13:29:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ WIN_COMBINATIONS = [
 
 # Define the won method
 def won?(board)
-  WIN_COMBINATIONS.each do win_combination
+  WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
